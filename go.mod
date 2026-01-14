75598fe5f669eb0718c7f21a4f6ac1953f02c3a6
Miguel Filipe <miguel.filipe@gmail.com>
2026-01-14 23:00:02 +0000

    publish readings to mqtt

--- go.mod
+++ go.mod
@@ -2,13 +2,18 @@ module iam-t1-exporter
 
 go 1.25
 
-require tinygo.org/x/bluetooth v0.9.0
+require (
+	github.com/eclipse/paho.golang v0.23.0
+	tinygo.org/x/bluetooth v0.9.0
+)
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/saltosystems/winrt-go v0.0.0-20240320113951-a2e4fc03f5f4 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tinygo-org/cbgo v0.0.4 // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"os"
@@ -9,6 +10,11 @@ import (
 	"syscall"
 	"time"
 
+	"net/url"
+
+	"github.com/eclipse/paho.golang/autopaho"
+	"github.com/eclipse/paho.golang/autopaho/queue/memory"
+	"github.com/eclipse/paho.golang/paho"
 	"tinygo.org/x/bluetooth"
 )
 
@@ -20,17 +26,24 @@ type config struct {
 }
 
 type Reading struct {
-	Timestamp uint32 // unix seconds (valid until 2106)
-	CO2       uint16 // ppm
-	Humidity  uint16 // tenths of percent (720 = 72.0%)
-	Pressure  uint16 // hPa
+	Timestamp   uint32  `json:"timestamp"` // unix seconds (valid until 2106)
+	CO2         uint16  `json:"co2_ppm"`
+	Pressure    uint16  `json:"pressure_hPa"` // hPa  uint16 // hPa
+	Humidity    float32 `json:"humidity"`
+	Temperature float32 `json:"temperature"` // Celsius
 }
 
 func (r Reading) String() string {
-	ts := time.Unix(int64(r.Timestamp), 0).Format("15:04:05")
-	humidity := float64(r.Humidity) / 10.0
-	return fmt.Sprintf("%s | CO2: %4d ppm | Humidity: %4.1f%% | Pressure: %4d hPa",
-		ts, r.CO2, humidity, r.Pressure)
+	ts := time.Unix(int64(r.Timestamp), 0).Format(time.RFC3339)
+	return fmt.Sprintf("%s | CO2: %4d ppm | Humidity: %4.1f%% | Pressure: %4d hPa | Temperature: %4.1fC",
+		ts, r.CO2, r.Humidity, r.Pressure, r.Temperature)
+}
+
+type MQTTConfig struct {
+	ServerURL string
+	ClientID  string
+	Username  string
+	Password  string
 }
 
 func main() {
@@ -39,8 +52,20 @@ func main() {
 		deviceAddr:      "62:00:A1:3F:B4:26",
 		sensorServiceID: "0000ffe0-0000-1000-8000-00805f9b34fb",
 		notifyCharID:    "0000ffe4-0000-1000-8000-00805f9b34fb",
-		channelSize:     10000,
+		channelSize:     1,
 	}
+	// parse configs from env
+	mqttConfig := MQTTConfig{
+		ClientID:  "inkbird-iam-t1_" + time.Now().UTC().Format(time.RFC3339),
+		ServerURL: os.Getenv("MQTT_SERVER"),
+		Username:  os.Getenv("MQTT_USERNAME"),
+		Password:  os.Getenv("MQTT_PASSWORD"),
+	}
+	mqtt, err := NewMQTT(context.Background(), mqttConfig, logger)
+	assert("mqtt", err)
+	defer func() {
+		assert("mqtt close", mqtt.Close())
+	}()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -59,7 +84,7 @@ func main() {
 	}()
 
 	// Normal operation
-	go collectReadings(ctx, logger, readings)
+	go collectReadings(ctx, logger, readings, *mqtt)
 
 	// Shutdown handling
 	sig := make(chan os.Signal, 1)
@@ -81,14 +106,14 @@ func main() {
 	logger.Info("shutdown complete")
 }
 
-func registerDevice(logger *slog.Logger, adapter *bluetooth.Adapter, cfg config, readings chan<- Reading) bluetooth.Device {
-	logger.Info("scanning for device", "addr", cfg.deviceAddr)
+func registerDevice(log *slog.Logger, adapter *bluetooth.Adapter, cfg config, readings chan<- Reading) bluetooth.Device {
+	log.Info("scanning for bluetooth device", "addr", cfg.deviceAddr)
 	ch := make(chan bluetooth.ScanResult, 1)
 
 	err := adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
 		if result.Address.String() == cfg.deviceAddr {
 			if err := adapter.StopScan(); err != nil {
-				logger.Error("stop scan failed", "error", err)
+				log.Error("stop scan failed", "error", err)
 			}
 			ch <- result
 		}
@@ -96,11 +121,11 @@ func registerDevice(logger *slog.Logger, adapter *bluetooth.Adapter, cfg config,
 	assert("scan", err)
 
 	result := <-ch
-	logger.Info("found device, connecting")
+	log.Debug("found bluetooth device, connecting", "addr", result.Address)
 
 	device, err := adapter.Connect(result.Address, bluetooth.ConnectionParams{})
 	assert("connect", err)
-	logger.Info("connected")
+	log.Info("connected to bluetooth device", "addr", result.Address)
 
 	// Discover services
 	svcUUID := parseUUID(cfg.sensorServiceID)
@@ -108,27 +133,34 @@ func registerDevice(logger *slog.Logger, adapter *bluetooth.Adapter, cfg config,
 	assert("discover services", err)
 
 	for _, service := range services {
-		logger.Debug("discovered service", "uuid", service.UUID().String())
+		log.Debug("discovered service", "uuid", service.UUID().String())
 
+		// TODO:what more to discover?
 		charUUID := parseUUID(cfg.notifyCharID)
 		chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{charUUID})
 		assert("discover characteristics", err)
 
 		for _, char := range chars {
-			logger.Debug("discovered characteristic", "uuid", char.UUID().String())
+			log.Info("discovered characteristic", "uuid", char.UUID().String())
 
 			if char.UUID().String() == cfg.notifyCharID {
 				err = char.EnableNotifications(func(buf []byte) {
+					//TODO: discover whats on the first 3 bytes
+					someRaw := int16(buf[2])<<8 | int16(buf[3])
+					some := float32(someRaw) / 10.0
+					log.Info("bluetooth notification", "rawData", buf, "some", some)
 					if reading, ok := parseReading(buf); ok {
 						select {
 						case readings <- reading:
 						default:
-							logger.Warn("readings channel full, dropping data")
+							log.Warn("readings channel full, dropping data")
 						}
+					} else {
+						log.Warn("parseReadin failed", "data", buf)
 					}
 				})
 				assert("enable notifications", err)
-				logger.Info("notifications enabled")
+				log.Info("notifications enabled", "notification", char.UUID().String())
 			}
 		}
 	}
@@ -136,13 +168,20 @@ func registerDevice(logger *slog.Logger, adapter *bluetooth.Adapter, cfg config,
 	return device
 }
 
-func collectReadings(ctx context.Context, logger *slog.Logger, readings chan Reading) {
+func collectReadings(ctx context.Context, log *slog.Logger, readings chan Reading, mqtt mqttSession) {
 	for {
 		select {
 		case <-ctx.Done():
+			log.Info("context done")
 			return
 		case reading := <-readings:
-			logger.Info("reading", "data", reading.String())
+			log.Info("received reading", "data", reading.String())
+			body, err := json.Marshal(reading)
+			if err != nil {
+				log.Error("json marshal failed", "error", err)
+			} else if err := mqtt.Publish(ctx, "inkbird-iam-t1/reading", body); err != nil {
+				log.Error("mqtt publish failed", "error", err)
+			}
 		}
 	}
 }
@@ -152,11 +191,17 @@ func parseReading(data []byte) (Reading, bool) {
 		return Reading{}, false
 	}
 
+	temperatureRaw := int16(data[5])<<8 | int16(data[6])
+	temperature := float32(temperatureRaw) / 10.0
+
+	humidityRaw := uint16(data[7])<<8 | uint16(data[8])
+	humidity := float32(humidityRaw) / 10.0
 	return Reading{
-		Timestamp: uint32(time.Now().Unix()),
-		Humidity:  uint16(data[7])<<8 | uint16(data[8]),
-		CO2:       uint16(data[9])<<8 | uint16(data[10]),
-		Pressure:  uint16(data[11])<<8 | uint16(data[12]),
+		Timestamp:   uint32(time.Now().Unix()),
+		CO2:         uint16(data[9])<<8 | uint16(data[10]),
+		Pressure:    uint16(data[11])<<8 | uint16(data[12]),
+		Humidity:    humidity,
+		Temperature: temperature,
 	}, true
 }
 
@@ -171,4 +216,69 @@ func assert(action string, err error) {
 		slog.Error("fatal error", "action", action, "error", err)
 		os.Exit(1)
 	}
+}
+
+type mqttSession struct {
+	conn *autopaho.ConnectionManager
+}
+
+func NewMQTT(ctx context.Context, config MQTTConfig, log *slog.Logger) (*mqttSession, error) {
+	var err error
+	serverURL, err := url.Parse(config.ServerURL)
+	if err != nil {
+		return nil, err
+	}
+
+	cliCfg := autopaho.ClientConfig{
+		Queue:                         memory.New(),
+		ServerUrls:                    []*url.URL{serverURL},
+		ConnectUsername:               config.Username,
+		ConnectPassword:               []byte(config.Password),
+		KeepAlive:                     20, // Keepalive message should be sent every 20 seconds
+		CleanStartOnInitialConnection: true,
+		SessionExpiryInterval:         60, // If connection drops we want session to remain live whilst we reconnect
+		OnConnectionUp: func(cm *autopaho.ConnectionManager, connAck *paho.Connack) {
+			log.Info("mqtt: connection up", "server", config.ServerURL)
+		},
+		OnConnectError: func(err error) { log.Error("publish: error whilst attempting connection", "error", err) },
+		// TODO: how do I just use slog here?
+		// Errors:         logger{prefix: "publish"},
+		// Debug:          logger{prefix: "publish: debug"},
+		// PahoErrors: logger{prefix: "publishP"},
+		// PahoDebug:      logger{prefix: "publishP: debug"},
+		// eclipse/paho.golang/paho provides base mqtt functionality, the below config will be passed in for each connection
+		ClientConfig: paho.ClientConfig{
+			ClientID:      config.ClientID,
+			OnClientError: func(err error) { log.Error("publish: client error", "error", err) },
+			OnServerDisconnect: func(d *paho.Disconnect) {
+				if d.Properties != nil {
+					log.Info("publish: server requested disconnect", "reason", d.Properties.ReasonString)
+				} else {
+					log.Info("publish: server requested disconnect", "reason", d.ReasonCode)
+				}
+			},
+		},
+	}
+
+	c, err := autopaho.NewConnection(ctx, cliCfg)
+	if err != nil {
+		return nil, err
+	}
+	return &mqttSession{conn: c}, nil
+
+}
+
+func (s *mqttSession) Close() error {
+	return s.conn.Disconnect(context.Background())
+}
+
+func (s *mqttSession) Publish(ctx context.Context, topic string, msg []byte) error {
+	const AtLeastOnce = byte(1)
+	return s.conn.PublishViaQueue(ctx, &autopaho.QueuePublish{
+		Publish: &paho.Publish{
+			QoS:     AtLeastOnce,
+			Topic:   topic,
+			Payload: msg,
+		}},
+	)
 }
